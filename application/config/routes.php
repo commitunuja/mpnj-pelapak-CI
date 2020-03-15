78b1e054a84eeb602e6f50bce51902b2710c38f2
Ilham Surya Pratama <ilhamsuryapratama.github@gmail.com>
2020-03-15 13:40:45 +0700

    custom route tambah produk

--- application/config/routes.php
+++ application/config/routes.php
@@ -4,3 +4,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['pelapak/produk/tambah'] = 'Pelapak/produk_form';
